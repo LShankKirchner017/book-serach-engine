afe9a0bb38584ba6f141f1f859fed292e01362ea
Lindsay <lshank017@gmail.com>
2023-11-05 09:25:47 -0600

    ApolloClient, queries, mutations

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,36 @@
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client'
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import Auth from './utils/auth'
+
+const httpLink = createHttpLink({
+  url: '/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = Auth.getToken()
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+
+})
+
+const client = new ApolloClient ({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,81 @@
+import { gql } from 'graphql'
+
+export const CREATE_USER = gql `
+mutation CreateUser($user: userInput) {
+    createUser(user: $user) {
+        token
+        user {
+            _id
+            email
+            username 
+            savedBooks {
+                _id
+                authors 
+                description 
+                bookId 
+                image 
+                link 
+                title 
+            }
+        }
+    }
+}
+`
+
+export const LOGIN = gql `
+mutation Login($password: String!, $username: String, $email: String) {
+    login(password: $password, username: $username, email: $email) {
+        token,
+        user {
+            _id
+            email 
+            username 
+            savedBooks {
+                _id 
+                authors 
+                description 
+                bookId 
+                image 
+                link 
+                title 
+            }
+        }
+    }
+}
+`
+export const SAVE_BOOK = gql `
+mutation saveBook($_id: ID!, $book: BookInput!) {
+    saveBook(_id: $_id, book: $book) {
+        _id
+        email 
+        username 
+        savedBooks {
+            _id 
+            authors 
+            bookId 
+            description 
+            image 
+            link 
+            title 
+        }
+    }
+}
+`
+
+export const DELETE_BOOK = gql `
+mutation deleteBook($_id: ID!, $bookId: ID!) {
+    deleteBook(_id: $_id, bookId: $bookId) {
+        _id 
+        email 
+        savedBooks {
+            _id 
+            authors 
+            bookId 
+            description 
+            image 
+            link 
+            title 
+        }
+    }
+}
+`
